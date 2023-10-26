d6fb975d8a88d4582dcdc4b01c4297ac5169287d
Ashkie Esperanza <ashkiecharles@gmail.com>
2023-10-26 07:56:48 -0600

    added comments on javascript code

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 function Menu(e){
   /* TO be able to pull down the menu when user clicks*/
   let list = document.querySelector('ul');
-
+  /* e is the variable name of the class for the button. Here we use the name of icon of menu and simply state to change the class into close to change the hamburger menu into an X after the user has clicked on it. I have also changed the css through javascript by adding top px to provide a scrolling down mechanic with the list.*/
   e.name == 'menu' ? (e.name = "close", list.classList.add('top-[80px]'), list.classList.add('opacity-100')) : (e.name = "menu" ,list.classList.remove('top-[80px]'), list.classList.remove('opacity-0'))
 }
