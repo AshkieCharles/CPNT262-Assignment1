ef65ddb15ed36c998f093d31b4f749e428f081da
Ashkie Esperanza <ashkiecharles@gmail.com>
2023-10-26 08:52:06 -0600

    added comments to js

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ function Menu(e){
   /* e is the variable name of the class for the button. Here we use the name of icon of menu and simply state to change the class into close to change the hamburger menu into an X after the user has clicked on it. I have also changed the css through javascript by adding top px to provide a scrolling down mechanic with the list.*/
   e.name == 'menu' ? (e.name = "close", list.classList.add('top-[80px]'), list.classList.add('opacity-100')) : (e.name = "menu" ,list.classList.remove('top-[80px]'), list.classList.remove('opacity-0'))
 }
-
+/images/1.jpg
 for (i = 1; i <= 9; i++){
   /* takes the id of the div and create 9 different images. */
   Images.innerHTML += "<div><img src= /images/"+i+".jpg></div>";
